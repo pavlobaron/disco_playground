a1806f95798504302e47652fedc954fa0e473fa1
pavlobaron <pb@pbit.org>
2012-11-28 08:50:06 +0100

    queue based streaming implemented, more tests needed

--- master/src/erl/riak_stream.erl
+++ master/src/erl/riak_stream.erl
@@ -7,7 +7,7 @@
 -record(queue, {name,
                 url,
                 connection,
-                channel).
+                channel}).
 
 -include("amqp_client.hrl").
 
@@ -16,8 +16,8 @@ local_vnodes_stream(Bucket) ->
     VNodes = riak_core_vnode_manager:all_vnodes(),
     Preflist = [{X, Y} || {_, X, Y} <- VNodes],
     BBucket = list_to_binary(Bucket),
-    Queue = create_queue(Bucket),
-    spawn(?MODULE, receiver_loop, [self(), length(Preflist), BBucket, [], Queue]),
+    Queue = create_queue(Bucket),    
+    Receiver = spawn(?MODULE, receiver_loop, [length(Preflist), BBucket, [], Queue]),
     spawn_senders(Preflist, length(Preflist), Receiver, BBucket),
     %% TODO: more than one receiver, probably one per vnode
     Queue#queue.url.
@@ -26,27 +26,32 @@ local_vnodes_stream(Bucket) ->
 %% internals
 
 create_queue(Bucket) ->
-    QueueName = "riak_" ++ Bucket ++ "_" ++ atom_to_list(node()),
+    SQueueName = "riak_" ++ Bucket ++ "_" ++ atom_to_list(node()),
+    QueueName = list_to_binary(SQueueName),
     {ok, Connection} = amqp_connection:start(#amqp_params_network{}),
     {ok, Channel} = amqp_connection:open_channel(Connection),
-    #'queue.declare_ok'{queue=QueueName} = amqp_channel:call(Channel, #'queue.declare'{}),
-    Url = "queue://" ++ QueueName,
+    #'queue.declare_ok'{queue=QueueName} = amqp_channel:call(Channel,
+                                                 #'queue.declare'{queue = QueueName,
+                                                                 durable = true,
+                                                                 exclusive = false,
+                                                                 auto_delete = false}),
+    Url = "queue://" ++ SQueueName,
     #queue{name=QueueName, url=Url, connection=Connection, channel=Channel}.
 
 publish(Queue, Data) ->
-    Publish = #'basic.publish'{exchange=<<>>, routing_key = Queue#queue.name},
-    amqp_channel:cast(Queue#queue.channel, Publish, #amqp_msg{payload=Data}),
+    Publish = #'basic.publish'{exchange = <<"">>, routing_key = Queue#queue.name},
+    amqp_channel:cast(Queue#queue.channel, Publish, #amqp_msg{payload = Data}).
 
--spec receiver_loop(pid(), integer(), binary(), term()) -> ok.
-receiver_loop(Owner, 0, Bucket, VNodeKeys, Queue) ->
-    Max = 4,%length(VNodeKeys),
+-spec receiver_loop(integer(), binary(), term(), record()) -> ok.
+receiver_loop(0, Bucket, VNodeKeys, Queue) ->
+    Max = length(VNodeKeys),
     read_data(VNodeKeys, Max, Bucket, Queue);
-receiver_loop(Owner, Count, Bucket, VNodeKeys, Queue) ->
+receiver_loop(Count, Bucket, VNodeKeys, Queue) ->
     receive
         {_, []} ->
-            receiver_loop(Owner, Count - 1, Bucket, VNodeKeys, Queue);
+            receiver_loop(Count - 1, Bucket, VNodeKeys, Queue);
         {VNode, [{_, {_, _, Keys}}]} ->
-            receiver_loop(Owner, Count - 1, Bucket,
+            receiver_loop(Count - 1, Bucket,
                           [{VNode, lists:flatten(Keys)}|VNodeKeys], Queue)
     end.
 
@@ -55,24 +60,24 @@ read_data(_, 0, _, _) ->
 read_data([{VNode, Keys}|T], RefId, Bucket, Queue) ->
     BucketKeys = [{Bucket, Key} || Key <- Keys],
     ok = riak_kv_vnode:mget(VNode, BucketKeys, RefId),
-    Max = 16,%length(Keys),
-    Data = read_data_loop(Data, Max), %TODO: refactor to async, backpressure possible
-    publish(Queue, Data)
+    Max = length(Keys),
+    read_data_loop(Max, Queue), %TODO: refactor to async, backpressure possible
     read_data(T, RefId - 1, Bucket, Queue).
 
-read_data_loop(Data, 0) ->
-    Data;
-read_data_loop(Data, Counter) ->
+read_data_loop(0, _) ->
+    ok;
+read_data_loop(Counter, Queue) ->
     receive
         {_, {_, {r_object, _, _, [{r_content, _, Object}], _, _, _}, _, _}} ->
             %TODO: refactor to add a dummy vector clock, use riakc (but siblings?..)
-            read_data_loop([Object, "\n"|Data], Counter - 1);
+            publish(Queue, Object),
+            read_data_loop(Counter - 1, Queue);
         Dunno ->
             error_logger:error_msg("what is this? ~p~n", [Dunno]),
-            read_data_loop(Data, Counter - 1)
+            read_data_loop(Counter - 1, Queue)
     after
         500 ->
-            read_data_loop(Data, Counter - 1)
+            read_data_loop(Counter - 1, Queue)
     end.
 
 spawn_senders(_, 0, _, _) ->
